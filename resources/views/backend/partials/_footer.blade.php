cb181601982ed3d9a01be5fa195fb35c6b5788a5
unknown <itarif.diit@gmail.com>
2021-10-18 16:31:04 +0600

    Footer issue solved

--- resources/views/backend/partials/_footer.blade.php
+++ resources/views/backend/partials/_footer.blade.php
@@ -1,5 +1,7 @@
-<footer class="main-footer">
-  <strong>Copyright &copy; 2020-2021 <a href="http://entertechbd.com/">Entertech</a>.</strong>
-  All rights reserved.
-  <div class="float-right d-none d-sm-inline-block">  </div>
-</footer>
+<div id="container" style="position: relative;">
+  <footer class="main-footer" style="position: absolute;bottom: 0;">
+    <strong>Copyright &copy; 2020-2021 <a href="http://entertechbd.com/">Entertech</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">  </div>
+  </footer>
+</div>
